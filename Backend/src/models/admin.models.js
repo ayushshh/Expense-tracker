4ab773eec72b1a9df903ac2d3010976f469a4363
xfgryv <bs9882074@gmail.com>
2025-10-28 23:41:52 +0530

    models completed for admin and user

---
+++ Backend/src/models/admin.models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { Schema } = "mongoose";
+
+const adminSchema = new Schema({
+    username: {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password: {
+        type : String,
+        required : true
+    },
+    firstName: {
+        type : String,
+        required : true
+    },
+    lastName: {
+        type : String,
+        required : true
+    },
+    email: {
+        type : String,
+        required : true,
+        unique : true
+    }
+}, { timestamps: true });
+
+const Admin = mongoose.model('Admin', adminSchema);
+module.exports = Admin;
---
+++ Backend/src/models/user.models.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password: {
+        type : String,
+        required : true
+    },
+    firstName: {
+        type : String,
+        required : true
+    },
+    lastName: {
+        type : String,
+        required : true
+    },
+    email: {
+        type : String,
+        required : true,
+        unique : true
+    }
+}, { timestamps: true });
+const User = mongoose.model('User', userSchema);
+module.exports = User;
